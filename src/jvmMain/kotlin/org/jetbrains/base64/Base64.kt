fcf56be4146553dd4f667ddc0c3d1e1ba94613a8
Onix Wang <ories.wang@gmail.com>
2021-08-07 21:06:21 +0100

    added implementation

---
+++ src/jvmMain/kotlin/org/jetbrains/base64/Base64.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.base64
+
+import java.util.*
+
+actual object Base64Factory{
+    actual fun createEncoder(): Base64Encoder = JvmBase64Encoder
+}
+
+object JvmBase64Encoder : Base64Encoder {
+    override fun encode(src: ByteArray): ByteArray {
+        return  Base64.getEncoder().encode(src)
+    }
+}
